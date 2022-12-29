0200ae28a3d2e26447ad2627ee3248090c608e4d
alpertas <alpertas.cpp@gmail.com>
2022-12-29 14:08:40 +0300

    initialize tailwind&&folder structure

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import Header from './Header';
+import Hero from './Hero';
+import Services from './Services';
+import Partners from './Partners';
+import Team from './Team';
+import GetStart from './GetStart';
+import Footer from './Footer';
+
+export { Header, Hero, Services, Partners, Team, GetStart, Footer };
